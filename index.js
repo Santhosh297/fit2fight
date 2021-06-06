101186c1a870cd9fe7a6d699f1f7dec3fcae34a1
Santhosh297 <76590065+Santhosh297@users.noreply.github.com>
2021-06-06 12:14:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function () {
+    $("#review .owl-carousel").owlCarousel({
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        loop: true,
+        items: 1
+    });
+});
